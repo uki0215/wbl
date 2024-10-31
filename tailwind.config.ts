76d38de9ef14e15d7a8b78022e793b8eaa48f841
Ulambayar Boldbaatar <andyuki@Ulambayars-MacBook-Pro.local>
2024-10-31 09:36:30 +0800

    tailwind color tohirgoo hiisen

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: ["class"],
+    darkMode: "class",
     content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,6 +11,26 @@ const config: Config = {
   theme: {
   	extend: {
   		colors: {
+					white: "#ffffff",
+					gray: {
+						100: "#f3f4f6",
+						200: "#e5e7eb",
+						300: "#d1d5db",
+						500: "#6b7280",
+						700: "#374151",
+						800: "#1f2937"
+					},
+					blue: {
+						200: "#93c5fd",
+						400: "#6085fa",
+						500: "#3b82f6"
+					},
+					"dark-bg": "#101214",
+					"dark-secondary": "#1d1f21",
+					"dark-tertiary": "#3b3d40",
+					"blue-primary": "#0275ff",
+					"stroke-dark": "#2d3135",
+					
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
